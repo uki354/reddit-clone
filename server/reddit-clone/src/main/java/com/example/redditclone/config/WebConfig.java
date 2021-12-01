f973881aa4ae107d61496cf79fa328e4e3e682ee
uki <uros.3.stevanovic@gmail.com>
2021-12-01 04:13:51 +0100

    cors

--- server/reddit-clone/src/main/java/com/example/redditclone/config/WebConfig.java
+++ server/reddit-clone/src/main/java/com/example/redditclone/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .maxAge(3600L)
                 .allowedHeaders("*")
