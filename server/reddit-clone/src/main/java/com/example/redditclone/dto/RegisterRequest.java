25b562d4207e68444608a6d0a03142ed2dace389
uki <uros.3.stevanovic@gmail.com>
2021-12-01 04:22:38 +0100

    fix

--- server/reddit-clone/src/main/java/com/example/redditclone/dto/RegisterRequest.java
+++ server/reddit-clone/src/main/java/com/example/redditclone/dto/RegisterRequest.java
@@ -2,9 +2,11 @@ package com.example.redditclone.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
 
     private String firstName;
